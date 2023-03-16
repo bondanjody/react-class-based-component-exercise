cea589a978f3610c29c49735ee0dc879232002a4
bondanjody <bondanjodys@gmail.com>
2023-03-16 15:07:55 +0700

    Menyempurnakan component ErrorBoundary dengan menampilkan suatu pesan jika terjadi error

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -6,12 +6,17 @@ class ErrorBoundary extends Component {
         this.state = { hasError: false };
     }
 
-    componentDidCatch() {
-
+    componentDidCatch(error) {
+        console.log(error);
+        this.setState({ hasError: true });
     }
 
     render() {
+        if (this.state.hasError) {
+            return <p>Something went wrong !</p>;
+        }
         return this.props.children;
     }
 }
 
+export default ErrorBoundary;
